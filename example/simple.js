ca42b8d51354697614a7843c994b7495f5942b73
vrunoa <vruno.alassia@gmail.com>
2015-02-20 00:22:04 -0300

    add random API method

--- example/simple.js
+++ example/simple.js
@@ -1,6 +1,11 @@
 var giphy = require('../node');
 
 giphy = giphy({api_key:'dc6zaTOxFJmzC'});
+
+/*
+ * Search API Example
+ *
+ *
 giphy.search({q:"flip the table"})
 .then(function(response){
   console.log("success");
@@ -9,4 +14,25 @@ giphy.search({q:"flip the table"})
   console.log("error");
   console.log(error);  
 });
+/*
+ * 
+ */
+
+
+/*
+ * Random API Example
+ *
+ */
+giphy.random({tag:"facepalm"})
+.then(function(response){
+  console.log("success");
+  console.log(response.data);
+}, function(error){
+  console.log("error");
+  console.log(error);  
+});
+
+/*
+ * 
+ */
 
